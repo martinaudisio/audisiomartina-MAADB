1721ba5aaadc75aca0a763d16ddb191e12be7e34
martinaudisio <martina.audisio@gmail.com>
2025-05-04 10:23:12 +0100

    Add routes to app

--- solution/neo4j-service/app.js
+++ solution/neo4j-service/app.js
@@ -20,7 +20,14 @@ app.listen(PORT, () => {
 
 
 const personRoutes = require('./routes/personRoutes');
+const postRoutes = require('./routes/postRoutes'); 
+const commentRoutes = require('./routes/commentRoutes'); 
+
+
+app.use('/api/post', postRoutes); 
 app.use('/api/people', personRoutes);
+app.use('/api/comment', commentRoutes);
+
 
 
 /**
