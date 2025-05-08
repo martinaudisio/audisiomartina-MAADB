3d253ffc40fa345112042ae55a775505f34286c9
martinaudisio <martina.audisio@gmail.com>
2025-05-08 17:51:22 +0100

    Add routes to app.js

--- solution/gateway-service/app.js
+++ solution/gateway-service/app.js
@@ -3,11 +3,34 @@
  * Sets up an Express server and connects to the Neo4j database using the configured driver.
  */
 
+const createError = require('http-errors');
 const express = require('express');
-const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+
+
 
 const bodyParser = require('body-parser');
+
+
+const app = express();
 app.use(express.json());
+
+app.use(cors());
+
+const personRouter = require('./routes/person'); 
+app.use('/person', personRouter);
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+const commentRouter = require('./routes/comment');
+app.use('/comment', commentRouter);
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 /**
  * Starts the Express HTTP server on the specified port.
@@ -17,5 +40,20 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
