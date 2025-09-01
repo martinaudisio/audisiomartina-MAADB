93ff3daf7218662a2e3786d003793d4b82203220
martinaudisio <martina.audisio@gmail.com>
2025-09-01 15:22:10 +0100

    Remove unused post functions

--- solution/mongo-service/controllers/postController.js
+++ solution/mongo-service/controllers/postController.js
@@ -3,91 +3,6 @@ const Comment = require('../models/comment');
 const Forum = require('../models/forum');
 
 /**
- * Get all posts
- * @route GET /posts
- * @description Retrieve all posts
- * @access Public
- */
-exports.getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.find();
-
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(500).json({ message: 'Error retrieving posts', error });
-  }
-};
-
-/**
- * Get a post by its ID
- * @route GET /posts/:id
- * @description Retrieve a single post by its ID
- * @access Public
- */
-exports.getPostById = async (req, res) => {
-  try {
-    const post = await Post.findOne(
-      { id: Number(id) },
-      { id: 1, type: 1, creationDate: 1, content: 1, _id: 0 }
-    );
-    if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
-    }
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json({ message: 'Error retrieving post by ID.', error });
-  }
-};
-
-
-/**
- * Get posts by CreatorPersonId
- * @route GET /posts/creator/:creatorPersonId
- * @description Retrieve posts by the CreatorPersonId, including forum title and sorted by creation date
- * @access Public
- */
-exports.getPostsByCreatorPersonId = async (req, res) => {
-  try {
-    console.log('Fetching posts for CreatorPersonId:', req.params.creatorPersonId);
-    const posts = await Post.find({ CreatorPersonId: Number(req.params.creatorPersonId) })
-      .sort({ creationDate: -1 });
-
-    if (posts.length === 0) {
-      return res.status(404).json({ message: 'No posts found for this creator ID.' });
-    }
-
-
-    const postsWithForumTitle = await Promise.all(posts.map(async (post) => {
-      const postObj = post.toObject();
-      if (!postObj.ContainerForumId) {
-        return {
-          ...postObj,
-          forumTitle: 'No forum associated',
-        };
-      }
-      try {
-        const forum = await Forum.findOne({ id: Number(postObj.ContainerForumId) });
-        return {
-          ...postObj,
-          forumTitle: forum ? forum.title : 'Forum not found',
-        };
-      } catch (err) {
-        console.error('Error fetching forum:', err);
-        return {
-          ...postObj,
-          forumTitle: 'Error fetching forum',
-        };
-      }
-    }));
-
-    res.status(200).json(postsWithForumTitle);
-  } catch (error) {
-    console.error('Error retrieving posts by creator:', error);
-    res.status(500).json({ message: 'Error retrieving posts by creator ID.' });
-  }
-};
-
-/**
  * Retrieves all content (posts and comments) created by a specific person, ordered by creation date.
  * 
  * @async
@@ -178,26 +93,6 @@ exports.getContentByCreatorPersonId = async (req, res) => {
 };
 
 
-
-/**
- * Get posts by LocationCountryId
- * @route GET /posts/location/:locationCountryId
- * @description Retrieve posts by LocationCountryId
- * @access Public
- */
-exports.getPostsByLocationCountryId = async (req, res) => {
-  try {
-    const posts = await Post.find({ LocationCountryId: Number(req.params.locationCountryId) });
-    if (posts.length === 0) {
-      return res.status(404).json({ message: 'No posts found for this location country ID.' });
-    }
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(500).json({ message: 'Error retrieving posts by location ID.', error });
-  }
-};
-
-
 /**
  * Retrieves the last post for a specific creator checking if was created after the beginning of a given year.
  *
@@ -253,22 +148,3 @@ exports.getPostsByCreatorAndDate = async (req, res) => {
 };
 
 
-
-/**
- * @description Retrieve posts associated with a specific Forum, identified by ContainerForumId.
- * @route GET /posts/forum/:containerForumId
- * @param {number} containerForumId - The ID of the forum container to filter posts by.
- */
-exports.getPostsByForumId = async (req, res) => {
-  const { containerForumId } = req.params;
-  try {
-    const posts = await Post.find({ ContainerForumId: containerForumId });
-    if (posts.length === 0) {
-      return res.status(404).json({ message: 'No posts found for the specified forum ID.' });
-    }
-    res.status(200).json(posts);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error occurred while retrieving posts by forum ID.' });
-  }
-};
--- solution/mongo-service/routes/postRoutes.js
+++ solution/mongo-service/routes/postRoutes.js
@@ -3,45 +3,12 @@ const router = express.Router();
 const postController = require('../controllers/postController'); 
 
 /**
- * @route GET /post
- * @description Retrieve all post from the database.
- * @access Public
- */
-router.get('/', postController.getAllPosts);
-
-/**
- * @route GET /post/:id
- * @description Retrieve a post by its ID.
- * @param {string} id - The ID of the post to retrieve.
- * @access Public
- */
-router.get('/:id', postController.getPostById);
-
-
-/**
  * @route GET /post/content/:creatorPersonId
  * @description Retrieve all the contents published by a creatorPersonId.
  * @param {string} id - The ID of the person to retrieve content by.
  * @access Public
  */
 router.get('/content/:creatorPersonId', postController.getContentByCreatorPersonId);
-
-/**
- * @route GET /post/creator/:creatorPersonId
- * @description Retrieve post created by a specific CreatorPersonId.
- * @param {number} creatorPersonId - The ID of the creator person to filter by.
- * @access Public
- */
-router.get('/creator/:creatorPersonId', postController.getPostsByCreatorPersonId);
-
-/**
- * @route GET /post/location/:locationCountryId
- * @description Retrieve post from a specific location identified by LocationCountryId.
- * @param {number} locationCountryId - The ID of the location country to filter by.
- * @access Public
- */
-router.get('/location/:locationCountryId', postController.getPostsByLocationCountryId);
-
 
 /**
  * @route GET /post/creator/:id/date/:year
@@ -52,14 +19,5 @@ router.get('/location/:locationCountryId', postController.getPostsByLocationCoun
  */
 router.get('/creator/:id/date/:year', postController.getPostsByCreatorAndDate);
 
-
-
-/**
- * @route GET /post/forum/:containerForumId
- * @description Retrieve post associated with a specific Forum, identified by ContainerForumId.
- * @param {number} containerForumId - The ID of the forum container to filter post by.
- * @access Public
- */
-router.get('/forum/:containerForumId', postController.getPostsByForumId);
 
 module.exports = router;
