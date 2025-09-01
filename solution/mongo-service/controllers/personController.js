7e5673869e3aaac1f91b25cf45320c970c3865b9
martinaudisio <martina.audisio@gmail.com>
2025-09-01 15:21:47 +0100

    Remove unused person functions

--- solution/mongo-service/controllers/personController.js
+++ solution/mongo-service/controllers/personController.js
@@ -2,55 +2,6 @@ const { default: mongoose } = require('mongoose');
 const Person = require('../models/person'); 
 
 /**
- * Retrieves all people from the database.
- *
- * @async
- * @function getAllPeople
- * @param {Object} req - The HTTP request object.
- * @param {Object} res - The HTTP response object.
- * @returns {void} Sends a JSON response with all people or an error message.
- */
-exports.getAllPeople = async (req, res) => {
-  try {
-    const people = await Person.find(); 
-    res.status(200).json(people);
-  } catch (error) {
-    console.error('Error fetching people:', error);
-    res.status(500).json({
-      message: 'Error fetching people',
-      error: error.message
-    });
-  }
-};
-
-
-/**
- * Retrieves people by their location ID.
- *
- * @async
- * @function getPeopleByLocation
- * @param {Object} req - The HTTP request object.
- * @param {Object} res - The HTTP response object.
- * @returns {void} Sends a JSON response with all people or an error message.
- */
-exports.getPeopleByLocation = async (req, res) => {
-
-  const locationId = req.params.id;
-  console.log('Fetching people by location ID:', locationId);
-  try {
-    const people = await Person.find({ LocationCityId: Number(locationId)});
-    console.log('People found:', people); 
-    res.status(200).json(people);
-  } catch (error) {
-    console.error('Error fetching people:', error);
-    res.status(500).json({
-      message: 'Error fetching people',
-      error: error.message
-    });
-  }
-};
-
-/**
  * Retrieves a single person from the database by their ID.
  *
  * @async
--- solution/mongo-service/routes/personRoutes.js
+++ solution/mongo-service/routes/personRoutes.js
@@ -3,24 +3,10 @@ const router = express.Router();
 const personController = require('../controllers/personController');
 
 /**
- * @route GET /
- * @description Retrieve all people from the database.
- * @access Public
- */
-router.get('/', personController.getAllPeople);
-
-/**
  * @route GET /:id
  * @description Retrieve a single person by their ID.
  * @access Public
  */
 router.get('/:id', personController.getPersonById);
-
-/**
- * @route GET /byLocation/:id
- * @description Retrieve people by location ID.
- * @access Public
- */
-router.get('/byLocation/:id', personController.getPeopleByLocation);
 
 module.exports = router;
